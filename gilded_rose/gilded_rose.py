6439575ba0c82e986399684de03cf50473b9b9a8
NigeA <nige_a@hotmail.com>
2020-11-12 19:51:43 +0000

    Refactored version of GildedRose with stretch goal

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -3,16 +3,16 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def decrease_quality_based_on_quality_limit(self, item_quality, quality_limit):
+    def decrease_quality_based_on_quality_limit(self, item_quality, quality_limit, dec_amount=1):
         dec = 0
         if item_quality > quality_limit:
-            dec = 1
+            dec = dec_amount
         return dec
 
-    def decrease_quality_based_on_quality_and_sell_in_limits(self, item_quality, quality_limit, item_sell_in, sell_in_limit):
+    def decrease_quality_based_on_quality_and_sell_in_limits(self, item_quality, quality_limit, item_sell_in, sell_in_limit,dec_amount=1):
         dec = 0
         if item_sell_in < sell_in_limit and item_quality > quality_limit:
-            dec = 1
+            dec = dec_amount
         return dec
 
     def increase_quality_based_on_quality_limit(self, item_quality, quality_limit):
@@ -42,6 +42,13 @@ class GildedRose(object):
                     item.quality += self.increase_quality_based_on_quality_limit(item.quality, 50)
                     item.quality += self.increase_quality_based_on_quality_and_sell_in_limits(item.quality, 50, item.sell_in, 10)
                     item.quality += self.increase_quality_based_on_quality_and_sell_in_limits(item.quality, 50, item.sell_in, 5)
+            elif item.name == "Conjured Mana Cake":
+                item.quality -= self.decrease_quality_based_on_quality_limit(item.quality, 0, dec_amount=2)
+                if item.quality < 0: 
+                    item.quality = 0
+                item.quality -= self.decrease_quality_based_on_quality_and_sell_in_limits(item.quality, 0, item.sell_in, 0, dec_amount=2)
+                if item.quality < 0: 
+                    item.quality = 0                    
             elif item.name != "Sulfuras, Hand of Ragnaros":
                 item.quality -= self.decrease_quality_based_on_quality_limit(item.quality, 0)
                 item.quality -= self.decrease_quality_based_on_quality_and_sell_in_limits(item.quality, 0, item.sell_in, 0)
